4e0336ba4899e650d366cfbcfa731bf6b3b64b2e
Dejan Đekić <ddj@novalite.ns>
2015-11-18 14:13:44 +0100

    tweak commit

--- src/main/java/com/djex/example/WebSecurityConfig.java
+++ src/main/java/com/djex/example/WebSecurityConfig.java
@@ -7,14 +7,16 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 
 import com.google.common.base.Splitter;
 
 @Configuration
-// @EnableWebMvcSecurity
+@EnableWebSecurity
+@EnableGlobalMethodSecurity
 @PropertySource(value = "application.properties")
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
